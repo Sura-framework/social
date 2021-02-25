29ceefdb384986083c6b2045d0678d2ce6308433
Semen Alekseev <semyon492@ya.ru>
2021-02-25 22:25:40 +0300

    Update

--- app/Modules/AlbumsController.php
+++ app/Modules/AlbumsController.php
@@ -666,12 +666,10 @@ class AlbumsController extends Module{
      */
     public function save_pos_photos(): int
     {
-//        $tpl = Registry::get('tpl');
         $db = $this->db();
         $user_info = $this->user_info();
         $user_id = $user_info['user_id'];
         $logged = $this->logged();
-//        $lang = $this->get_langs();
 
         Tools::NoAjaxRedirect();
 
