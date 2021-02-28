44228d5f686966ba2fea5715fb7e2d4e48ad217f
Semen Alekseev <semyon492@ya.ru>
2021-02-28 21:01:42 +0300

    Update db queries

--- app/Models/News.php
+++ app/Models/News.php
@@ -17,12 +17,10 @@ class News
 	/**
 	 * @var Db|null
 	 */
-	private static ?Db $db;
 	private Connection $database;
 	
 	public function __construct()
 	{
-		self::$db = Db::getDB();
 		$this->database = Model::getDB();
 	}
 	
@@ -35,13 +33,12 @@ class News
 	 */
 	public function load_news(int $user_id, int $page, int $limit = 20): array
 	{
-		$db = Db::getDB();
-		return $db->super_query("SELECT tb1.ac_id, ac_user_id, action_text, action_time, action_type, obj_id, answer_text, link FROM `news` tb1 WHERE tb1.ac_user_id IN (SELECT tb2.friend_id FROM `friends` tb2
+		return $this->database->fetchAll("SELECT tb1.ac_id, ac_user_id, action_text, action_time, action_type, obj_id, answer_text, link FROM `news` tb1 WHERE tb1.ac_user_id IN (SELECT tb2.friend_id FROM `friends` tb2
                 WHERE user_id = '{$user_id}' AND tb1.action_type IN (1,2,3) AND subscriptions != 2) 
             OR 
                 tb1.ac_user_id IN (SELECT tb2.friend_id FROM `friends` tb2 
                 WHERE user_id = '{$user_id}' AND tb1.action_type = 11 AND subscriptions = 2) 
-            AND tb1.action_type IN (1,2,3,11)	ORDER BY tb1.action_time DESC LIMIT {$page}, 20", 1);
+            AND tb1.action_type IN (1,2,3,11)	ORDER BY tb1.action_time DESC LIMIT {$page}, 20");
 	}
 
     /**
@@ -174,7 +171,6 @@ class News
 		$cache = new Cache($storage, 'users');
 		$value = $cache->load("user_{$id}/votes/check{$user_id}_{$id}");
 		if ($value == null) {
-			$db = Db::getDB();
 			$row = $this->database->fetch("SELECT title, answers, answer_num FROM `votes` WHERE id = '{$id}'");
 			$value = serialize($row);
 			$cache->save("user_{$id}/votes/check{$user_id}_{$id}", $value);
@@ -211,7 +207,6 @@ class News
 	 */
 	public function user_tell_info(int $user_id, int $type): array
 	{
-		$db = Db::getDB();
 		if ($type == 1) {
 			return $this->database->fetch("SELECT user_search_pref, user_photo FROM `users` WHERE user_id = '{$user_id}'");
 		}
@@ -257,7 +252,6 @@ class News
 	 */
 	public function comments(int $id, int $limit): array
 	{
-		$db = Db::getDB();
 		return $this->database->fetchAll("SELECT tb1.id, author_user_id, text, add_date, tb2.user_photo, user_search_pref FROM `wall` tb1, `users` tb2 WHERE tb1.author_user_id = tb2.user_id AND tb1.fast_comm_id = '{$id}' ORDER by `add_date` LIMIT {$limit}, 3");
 	}
 	
