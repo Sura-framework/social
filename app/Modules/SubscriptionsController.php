722429ac9072f1c9a2d778fdce7f58e98b1af963
Semen Alekseev <semyon492@ya.ru>
2021-03-04 22:22:34 +0300

    update

--- app/Modules/SubscriptionsController.php
+++ app/Modules/SubscriptionsController.php
@@ -3,11 +3,14 @@
 namespace App\Modules;
 
 use App\Libs\Friends;
+use Sura\Cache\Cache;
+use Sura\Cache\Storages\MemcachedStorage;
 use Sura\Libs\Request;
 use Sura\Libs\Settings;
 use Sura\Libs\Status;
 use Sura\Libs\Tools;
 use Sura\Libs\Gramatic;
+use Sura\Time\Date;
 
 class SubscriptionsController extends Module{
 
@@ -31,7 +34,7 @@ class SubscriptionsController extends Module{
 
             $user_id = $user_info['user_id'];
 
-            $for_user_id = intval($request['for_user_id']);
+            $for_user_id = (int)$request['for_user_id'];
 
             //Проверка на существование юзера в подписках
             $check = $db->super_query("SELECT user_id FROM `friends` WHERE user_id = '{$user_id}' AND friend_id = '{$for_user_id}' AND subscriptions = 1");
@@ -48,21 +51,23 @@ class SubscriptionsController extends Module{
                         //Вставляем событие в моментальные оповещания
                         $row_owner = $db->super_query("SELECT user_last_visit, user_sex FROM `users` WHERE user_id = '{$for_user_id}'");
 
-                        $server_time = \Sura\Time\Date::time();
+                        $server_time = Date::time();
 
                         $update_time = $server_time - 70;
 
-                        $storage = new \Sura\Cache\Storages\MemcachedStorage('localhost');
-                        $cache = new \Sura\Cache\Cache($storage, 'users');
+                        $storage = new MemcachedStorage('localhost');
+                        $cache = new Cache($storage, 'users');
 
                         if($row_owner['user_last_visit'] >= $update_time){
 
                             $myRow = $db->super_query("SELECT user_sex FROM `users` WHERE user_id = '{$user_info['user_id']}'");
 
-                            if($myRow['user_sex'] == 1)
+                            if($myRow['user_sex'] == 1) {
                                 $action_update_text = 'подписался на Ваши обновления.';
-                            else
+                            }
+                            else {
                                 $action_update_text = 'подписалась на Ваши обновления.';
+                            }
 
                             $db->query("INSERT INTO `updates` SET for_user_id = '{$for_user_id}', from_user_id = '{$user_info['user_id']}', type = '13', date = '{$server_time}', text = '{$action_update_text}', user_photo = '{$user_info['user_photo']}', user_search_pref = '{$user_info['user_search_pref']}', lnk = '/u{$user_info['user_id']}'");
 
@@ -109,7 +114,7 @@ class SubscriptionsController extends Module{
 
             $request = (Request::getRequest()->getGlobal());
 
-            $del_user_id = intval($request['del_user_id']);
+            $del_user_id = (int)$request['del_user_id'];
 
             //Проверка на существование юзера в подписках
             $check = $db->super_query("SELECT user_id FROM `friends` WHERE user_id = '{$user_id}' AND friend_id = '{$del_user_id}' AND subscriptions = 1");
@@ -117,8 +122,8 @@ class SubscriptionsController extends Module{
                 $db->query("DELETE FROM `friends` WHERE user_id = '{$user_id}' AND friend_id = '{$del_user_id}' AND subscriptions = 1");
                 $db->query("UPDATE `users` SET user_subscriptions_num = user_subscriptions_num-1 WHERE user_id = '{$user_id}'");
 
-                $storage = new \Sura\Cache\Storages\MemcachedStorage('localhost');
-                $cache = new \Sura\Cache\Cache($storage, 'users');
+                $storage = new MemcachedStorage('localhost');
+                $cache = new Cache($storage, 'users');
 
                 //Чистим кеш
                 $cache->remove("{$user_id}/profile_{$user_id}");
@@ -155,11 +160,15 @@ class SubscriptionsController extends Module{
             $request = (Request::getRequest()->getGlobal());
 
             //################### Показ всех подпискок юзера ###################//
-            if($request['page'] > 0) $page = intval($request['page']); else $page = 1;
+            if($request['page'] > 0) {
+                $page = (int)$request['page'];
+            } else {
+                $page = 1;
+            }
             $gcount = 24;
             $limit_page = ($page-1)*$gcount;
-            $for_user_id = intval($request['for_user_id']);
-            $subscr_num = intval($request['subscr_num']);
+            $for_user_id = (int)$request['for_user_id'];
+            $subscr_num = (int)$request['subscr_num'];
 
             $sql_ = $db->super_query("SELECT tb1.friend_id, tb2.user_search_pref, user_photo, user_country_city_name, user_status FROM `friends` tb1, `users` tb2 WHERE tb1.user_id = '{$for_user_id}' AND tb1.friend_id = tb2.user_id AND tb1.subscriptions = 1 ORDER by `friends_date` DESC LIMIT {$limit_page}, {$gcount}", 1);
 
@@ -175,10 +184,12 @@ class SubscriptionsController extends Module{
                 $tpl->load_template('profile_friends.tpl');
                 $config = Settings::load();
                 foreach($sql_ as $row){
-                    if($row['user_photo'])
-                        $tpl->set('{ava}', $config['home_url'].'uploads/users/'.$row['friend_id'].'/50_'.$row['user_photo']);
-                    else
+                    if($row['user_photo']) {
+                        $tpl->set('{ava}', $config['home_url'] . 'uploads/users/' . $row['friend_id'] . '/50_' . $row['user_photo']);
+                    }
+                    else {
                         $tpl->set('{ava}', '/images/no_ava_50.png');
+                    }
                     $friend_info_online = explode(' ', $row['user_search_pref']);
                     $tpl->set('{user-id}', $row['friend_id']);
                     $tpl->set('{name}', $friend_info_online[0]);
@@ -192,8 +203,9 @@ class SubscriptionsController extends Module{
 //            $db->free();
 
             return view('info.info', $params);
-        } else
-            return view('info.info', $params);
+        }
+
+        return view('info.info', $params);
 
 
     }
